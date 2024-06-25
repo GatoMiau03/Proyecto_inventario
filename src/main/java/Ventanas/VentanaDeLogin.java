4f006bdf9e8c1e7e08bf9d91bfed45aa47e7a77a
Carlos <c.perez39@ufromail.cl>
2024-06-25 04:27:36 -0400

    Package Ventanas con los paneles del programa y package con las imagenes que ocuparemos.

---
+++ src/main/java/Ventanas/VentanaDeLogin.java
@@ -0,0 +1,10 @@
+package Ventanas;
+
+import javax.swing.*;
+
+public class VentanaDeLogin {
+    private JTextField textField1;
+    private JPanel panel1;
+    private JTextField textField2;
+    private JButton BotonIngresar;
+}
---
+++ src/main/java/Ventanas/VentanaInicioAdmin.java
@@ -0,0 +1,16 @@
+package Ventanas;
+
+import javax.swing.*;
+
+public class VentanaInicioAdmin {
+    private JButton eliminarProductoButton;
+    private JButton agregarVendedorButton;
+    private JButton agregarProductoButton;
+    private JButton mostrarInventarioButton;
+    private JButton quitarCantidadDeProductoButton;
+    private JButton eliminarVendedorButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
